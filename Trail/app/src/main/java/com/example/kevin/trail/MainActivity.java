3f651cd19d937646010e9b5f0ce92c2b1a2ec1ac
Kevin Yau <ky.kevinyau@gmail.com>
2017-03-08 12:03:34 -0500

    Added activities buttons and connected them to their own activity page for futher implementation.

--- Trail/app/src/main/java/com/example/kevin/trail/MainActivity.java
+++ Trail/app/src/main/java/com/example/kevin/trail/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.kevin.trail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Button runButtonlink =null;
+    protected Button hikeButtonlink =null;
+    protected Button bikeButtonlink =null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runButtonlink = (Button) findViewById(R.id.runButton);
+        hikeButtonlink = (Button) findViewById(R.id.hikeButton);
+        bikeButtonlink = (Button) findViewById(R.id.bikeButton);
+
+        runButtonlink.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View view){
+             goToRunActivity();
+         }
+        });
+
+        hikeButtonlink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                goToHikeActivity();
+            }
+        });
+
+        bikeButtonlink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                goToBikeActivity();
+            }
+        });
     }
+
+    void goToRunActivity(){
+        Intent intent = new Intent(MainActivity.this, runActivity.class);
+        startActivity(intent);
+    }
+    void goToHikeActivity(){
+        Intent intent = new Intent(MainActivity.this, hikeActivity.class);
+        startActivity(intent);
+    }
+
+    void goToBikeActivity(){
+        Intent intent = new Intent(MainActivity.this, bikeActivity.class);
+        startActivity(intent);
+    }
+
 }
