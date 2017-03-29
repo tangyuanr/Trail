d82b70d78a2f88c5ef5f9fee6f83a89e1d34d029
azfersheikh <mohammedazfer@hotmail.com>
2017-03-29 04:44:09 -0400

    I have made activity comparator but there are few things i need to change few changes added

--- Trail/app/src/main/java/com/example/kevin/trail/comparison.java
+++ Trail/app/src/main/java/com/example/kevin/trail/comparison.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class comparison extends AppCompatActivity {
-    EditText recentdistance , previousdistance , distancechange , recenttime , previoustime , timechange, previousspeed, recentspeed , speedchange , previouspace , recentpace , pacechange;
+    EditText recentdistance , previousdistance , distancechange , recenttime , previoustime , timechange, previousspeed, recentspeed , speedchange , previouspace , recentpace , pacechanges;
     DBHandler db = new DBHandler(this);
     ArrayList<Route> myroute;
     @Override
@@ -25,16 +25,16 @@ public class comparison extends AppCompatActivity {
     public void initializer(){
         recentdistance = (EditText) findViewById(R.id.recentdistance);
         previousdistance = (EditText) findViewById(R.id.previousdistance);
-        distancechange = (EditText) findViewById(R.id.distancechange);
+      //  distancechange = (EditText) findViewById(R.id.distancechange);
         recenttime = (EditText) findViewById(R.id.recenttime);
         previoustime = (EditText) findViewById(R.id.previoustime);
-        timechange = (EditText) findViewById(R.id.timechange);
+      //  timechange = (EditText) findViewById(R.id.timechange);
         previousspeed = (EditText) findViewById(R.id.previousspeed);
         recentspeed = (EditText) findViewById(R.id.recentspeed);
         speedchange = (EditText) findViewById(R.id.speedchange);
         previouspace = (EditText) findViewById(R.id.previouspace);
         recentpace = (EditText) findViewById(R.id.recentpace);
-        pacechange = (EditText) findViewById(R.id.pacechange);
+        pacechanges = (EditText) findViewById(R.id.pacechanges);
     }
 
     public void fetching(){
@@ -58,17 +58,17 @@ public class comparison extends AppCompatActivity {
                 if (myroute.size() >= 2) {
                     recentdistance.setText("" + myroute.get(myroute.size() - 1).getTotalDistance());
                     previousdistance.setText("" + myroute.get(myroute.size() - 2).getTotalDistance());
-                    distancechange.setText("" + ((myroute.get(myroute.size() - 1).getTotalDistance()) - (myroute.get(myroute.size() - 2).getTotalDistance())));
+                   // distancechange.setText("" + ((myroute.get(myroute.size() - 1).getTotalDistance()) - (myroute.get(myroute.size() - 2).getTotalDistance())));
                     recenttime.setText("" + myroute.get(myroute.size() - 1).getBestTime());
                     previoustime.setText("" + myroute.get(myroute.size() - 2).getBestTime());
-                    timechange.setText("" + ((myroute.get(myroute.size() - 1).getBestTime()) - (myroute.get(myroute.size() - 2).getTotalDistance())));
+                   // timechange.setText("" + ((myroute.get(myroute.size() - 1).getBestTime()) - (myroute.get(myroute.size() - 2).getTotalDistance())));
                     recentdistance.setText("" + myroute.get(myroute.size() - 1).getBestTime());
                     recentspeed.setText("" + (myroute.get(myroute.size() - 1).getTotalDistance()) / ((myroute.get(myroute.size() - 1).getBestTime())));
                     previousspeed.setText("" + (myroute.get(myroute.size() - 1).getTotalDistance()) / ((myroute.get(myroute.size() - 2).getBestTime())));
                     speedchange.setText("" + ((((myroute.get(myroute.size() - 1).getTotalDistance()) / ((myroute.get(myroute.size() - 1).getBestTime())))) - ((myroute.get(myroute.size() - 1).getTotalDistance()) / ((myroute.get(myroute.size() - 2).getBestTime())))));
                     previouspace.setText("" + (myroute.get(myroute.size() - 2).getBestTime()) / ((60000) * myroute.get(myroute.size() - 2).getTotalDistance()));//min per km
                     recentpace.setText("" + (myroute.get(myroute.size() - 1).getBestTime()) / ((60000) * myroute.get(myroute.size() - 1).getTotalDistance()));//min per km
-                    pacechange.setText("" + ((myroute.get(myroute.size() - 1).getBestTime()) / ((60000) * myroute.get(myroute.size() - 1).getTotalDistance()) - ((myroute.get(myroute.size() - 2).getBestTime()) / ((60000) * myroute.get(myroute.size() - 2).getTotalDistance()))));
+                    pacechanges.setText("" + ((myroute.get(myroute.size() - 1).getBestTime()) / ((60000) * myroute.get(myroute.size() - 1).getTotalDistance()) - ((myroute.get(myroute.size() - 2).getBestTime()) / ((60000) * myroute.get(myroute.size() - 2).getTotalDistance()))));
                 }
 
 
