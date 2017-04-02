f0070b50b62c9d9bc761358bc75627b8e54f3af3
ezekiel21 <eliaselias9191@gmail.com>
2017-04-02 12:02:11 -0400

    gfgd

--- Trail/app/src/main/java/com/example/kevin/trail/Route.java
+++ Trail/app/src/main/java/com/example/kevin/trail/Route.java
@@ -80,7 +80,7 @@ public class Route implements Serializable { //needed to be able to pass it betw
     }
 
     public float getTotalDistance() {
-        return totalDistance;
+        return round(totalDistance,1);
     }
 
     public int getBestTime() {
@@ -112,6 +112,12 @@ public class Route implements Serializable { //needed to be able to pass it betw
             return routeName + "\n Best time:\n" + bestTimeString + "\n on " + formatDate(dateBestTime) +"\n Distance: " + String.format("%.2f", totalDistance) + " km";
         }
         else {return null;}
+    }
+
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_DOWN);
+        return bd.floatValue();
     }
 
     private static String formatDate(String YYYYMMDD) {
